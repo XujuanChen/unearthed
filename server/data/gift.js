748d1cf7d807ab7586ba2afd2a8ef679d1f2ab78
XujuanChen <xchen022@citymail.cuny.edu>
2023-09-22 00:19:15 -0400

    step 3

---
+++ server/data/gift.js
@@ -0,0 +1,65 @@
+const giftData = [
+    {
+        "id": 1,
+        "name": "Halloween Horror Hand Prop",
+        "pricePoint": "$",
+        "audience": "Halloween Horror Hand Prop",
+        "image":"https://tinyurl.com/mryxcvvs",
+        "description":"Halloween Horror Hand Prop: Add A Spooky Touch To Your Costume With These Scary Props! Christmas Halloween Thanksgiving Gift",
+        "submittedBy":"Sasha",
+        "submittedOn":"2022-09-04T14:48:00"
+    },
+    {
+        "id": 2,
+        "name": "Halloween Graveyard Tombstone",
+        "pricePoint": "$",
+        "audience": "Halloween Graveyard Tombstone",
+        "image":"https://tinyurl.com/5925ajwh",
+        "description":"Halloween Graveyard Tombstone, Large Tombstone Halloween Decoration Yard Sign With Bamboo Skewers, High Realistic Scary Skeleton RIP Tombstone Outdoor Yard Lawn, Scene Decor, Room Decor, Home Decor, Holiday Party Decor, Halloween Decor",
+        "submittedBy":"Isabel",
+        "submittedOn":"2023-02-14T12:32:00"
+    },
+    {
+        "id": 3,
+        "name": "Electric Luminous Hanging Ghost",
+        "pricePoint": "$$",
+        "audience": "Gamers",
+        "image":"https://tinyurl.com/2z8kpswy",
+        "description":"Halloween Decoration Witch Voice Control Props Ghost Festival Haunted House Bar Toys Horror Toys Electric Luminous Hanging Ghost",
+        "submittedBy":"Mario",
+        "submittedOn":"2022-09-13T11:01:00"
+    },
+    {
+        "id": 4,
+        "name": "Halloween Ghost Pumpkin",
+        "pricePoint": "$$",
+        "audience": "Sneakerheads",
+        "image":"https://tinyurl.com/4k4m2zp9",
+        "description":"Halloween Ghost Pumpkin Decor Yard Stakes Prop For Lawn Decorations Outdoor Garden Home Party Decor",
+        "submittedBy":"Jason",
+        "submittedOn":"2023-03-10T05:23:00"
+    },
+    {
+        "id": 5,
+        "name": "Lighted Black Halloween Tree",
+        "pricePoint": "$$",
+        "audience": "Gamers",
+        "image":"https://tinyurl.com/2s35pej8",
+        "description":"Lighted Black Halloween Tree For Tabletop, 2FT Spooky Tree With LED Orange/purple Lights, Battery Operated Halloween Tree With Lights For Halloween Decorations Indoor Home",
+        "submittedBy":"Pixie",
+        "submittedOn":"2011-10-10T14:48:00"
+    },
+    {
+        "id": 6,
+        "name": "Realistic Skeleton Head And Hand Arm",
+        "pricePoint": "$",
+        "audience": "Sungazers",
+        "image":"https://tinyurl.com/4cnthey3",
+        "description":"Realistic Skeleton Head And Hand Arms For Halloween Party Decor - Perfect For Haunted House And Lawn Decor - Scary And Realistic Props",
+        "submittedBy":"Melody",
+        "submittedOn":"2011-10-10T14:48:00"
+    },
+
+]
+
+export default giftData
